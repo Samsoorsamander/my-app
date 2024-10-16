24d59e2fff346fa20b09efa7c597a6032483c171
Samsoorsamander <samsoorsamander@gmail.com>
2024-10-16 18:37:45 +0430

    React Native Buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,132 +1,44 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
-import type {PropsWithChildren} from 'react';
+import React, {Component} from 'react';
+import {Alert, Button, StyleSheet, View} from 'react-native';
 
-const PositionLayout = () => {
-  const [position, setPosition] = useState<'relative' | 'absolute'>('relative');
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
 
-  return (
-    <PreviewLayout
-      label="position"
-      selectedValue={position}
-      values={['relative', 'absolute']}
-      setSelectedValue={setPosition}>
-      <View
-        style={[
-          styles.box,
-          {
-            top: 25,
-            left: 25,
-            position,
-            backgroundColor: 'powderblue',
-          },
-        ]}
-      />
-      <View
-        style={[
-          styles.box,
-          {
-            top: 50,
-            left: 50,
-            position,
-            backgroundColor: 'skyblue',
-          },
-        ]}
-      />
-      <View
-        style={[
-          styles.box,
-          {
-            top: 75,
-            left: 75,
-            position,
-            backgroundColor: 'steelblue',
-          },
-        ]}
-      />
-    </PreviewLayout>
-  );
-};
-
-type PreviewLayoutProps = PropsWithChildren<{
-  label: string;
-  values: Array<'relative' | 'absolute'>;
-  selectedValue: string;
-  setSelectedValue: (value: 'relative' | 'absolute') => void;
-}>;
-
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}: PreviewLayoutProps) => (
-  <View style={{padding: 10, flex: 1}}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}>
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}>
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={styles.container}>{children}</View>
-  </View>
-);
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this._onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this._onPressButton} title="This looks great!" />
+          <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 8,
-    backgroundColor: 'aliceblue',
-    minHeight: 200,
+    justifyContent: 'center',
   },
-  box: {
-    width: 50,
-    height: 50,
+  buttonContainer: {
+    margin: 20,
   },
-  row: {
+  alternativeLayoutButtonContainer: {
+    margin: 20,
     flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: 'oldlace',
-    alignSelf: 'flex-start',
-    marginHorizontal: '1%',
-    marginBottom: 6,
-    minWidth: '48%',
-    textAlign: 'center',
-  },
-  selected: {
-    backgroundColor: 'coral',
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: '500',
-    color: 'coral',
-  },
-  selectedLabel: {
-    color: 'white',
-  },
-  label: {
-    textAlign: 'center',
-    marginBottom: 10,
-    fontSize: 24,
+    justifyContent: 'space-between',
   },
 });
-
-export default PositionLayout;

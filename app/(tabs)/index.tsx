4adf1b3ea63859f73e63145f6e18ce409be8ed38
Samsoorsamander <samsoorsamander@gmail.com>
2024-11-13 20:12:39 +0430

    .....

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,32 @@
-import * as React from 'react';
-import {View} from 'react-native'
-import LittleLemonHeader from '../../components/LittleLemonHeader';
-import MenuItems from '@/components/MenuItems';
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <View
-        style ={{
-            flex:1,
-            backgroundColor:'#495e57'
-        }}
-        >
-            <LittleLemonHeader />
-            <MenuItems />            
-        </View>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333333",
+  },
+  footerContainer: {
+    flex: 0.1,
+    backgroundColor: "#333333",
+  },
+});
